20ca76323e38854e93c93c4bf3f7985dabfd498c
Phumudzo Nthangeni <phumudzo.nthangeni@boxfusion.io>
2022-10-20 00:15:18 +0200

    dropEdit added

--- src/components/DropSlot/index.tsx
+++ src/components/DropSlot/index.tsx
@@ -1,42 +1,127 @@
-import { Input } from 'antd';
-import React ,{FC, useState} from 'react';
-import { useDrop } from 'react-dnd';
-import { IResolvedProps } from '../EverHourDrop/Recenttask';
-import { IItemProps } from '../item';
-import style from './style.module.scss'
+import { DatePicker, Form, Input, InputNumber, Mentions, Select } from "antd";
+import React, { FC, useState } from "react";
+import { FaProjectDiagram } from "react-icons/fa";
+import { MdDateRange } from "react-icons/md";
+import { MdWorkOutline } from "react-icons/md";
+import { BiTimer } from "react-icons/bi";
+import { useDrop } from "react-dnd";
+import { IResolvedProps } from "../EverHourDrop/Recenttask";
+import { IItemProps } from "../item";
+import timeSheet from "../timesheet.json";
+import style from "./style.module.scss";
 
-
-export interface IDropProps{
-    setResolvedItem?:(value:IResolvedProps)=>void;
-    resolvedItem?:IResolvedProps;
-    
+export interface IDropProps {
+  setResolvedItem?: (value: IResolvedProps) => void;
+  resolvedItem?: IResolvedProps;
 }
-export const DropSlot:FC<IDropProps>=({resolvedItem,setResolvedItem})=>{
 
-    const [{isOver},drop]=useDrop(()=>({
-        accept:'item',
-        drop:(item:IItemProps)=>{
-            setResolvedItem(({...resolvedItem,...item}))
-        },
-        collect:(monitor)=>({
-            isOver:!!monitor.isOver()
-        }),
-        canDrop:(item,monitor)=>{
-            return true
-        }
-    }));
+const { Option } = Mentions;
+export const DropSlot: FC<IDropProps> = ({ resolvedItem, setResolvedItem }) => {
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: "item",
+    drop: (item: IItemProps) => {
+      setResolvedItem({ ...resolvedItem, ...item });
+    },
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+    canDrop: (item, monitor) => {
+      return true;
+    },
+  }));
 
-    return (
-        
-        <div className={!isOver? style.dropContainer:style.isOverColumn}  ref={drop}>
-         <Input placeholder='Time Spent' 
-                      maxLength={2}  
-                      style={{width:'100%'}}
-                      onChange={({target:{value}})=>{
-                        if(!isNaN(parseInt(value)))setResolvedItem(({...resolvedItem,time:parseInt(value)}))}
-                      
-                    }/>
-        </div>
-    )
+  const recentTasks = timeSheet[0].result.map(({ id, projectName }) => {
+    return {
+      id,
+      projectName,
+    };
+  });
 
-}
+  return (
+    <div className={style.editForm}>
+      <div
+        className={!isOver ? style.dropContainer : style.isOverColumn}
+        ref={drop}
+      >
+        <Input
+          placeholder="Drop new workItems here"
+          maxLength={2}
+          style={{ width: "100%" }}
+          onChange={({ target: { value } }) => {
+            if (!isNaN(parseInt(value)))
+              setResolvedItem({ ...resolvedItem, time: parseInt(value) });
+          }}
+        />
+      </div>
+
+      <Form
+        labelCol={{ span: 4 }}
+        wrapperCol={{ span: 14 }}
+        layout="horizontal"
+        initialValues={{ size: 200 }}
+        style={{ padding: "1rem" }}
+      >
+        <Form.Item
+          label={
+            <span>
+              Work item <MdWorkOutline color="green" />
+            </span>
+          }
+          required
+          labelCol={{ span: 7 }}
+        >
+           <Mentions style={{ width: "100%" }} defaultValue="@345875">
+            <Option value="345875">345875</Option>
+            <Option value="275342">275342</Option>
+            <Option value="417840">417840</Option>
+          </Mentions>
+        </Form.Item>
+        <Form.Item
+          label={
+            <span>
+              Task <FaProjectDiagram color="green" />
+            </span>
+          }
+          required
+          labelCol={{ span: 7 }}
+        >
+          <Select>
+            {recentTasks.map(({ id, projectName }) => {
+              return <Select.Option value={id}>{projectName}</Select.Option>;
+            })}
+          </Select>
+
+        </Form.Item>
+        <Form.Item
+          label={
+            <span>
+              Date <MdDateRange color="green" />
+            </span>
+          }
+          required
+          labelCol={{ span: 7 }}
+        >
+          <DatePicker style={{ width: "100%" }} />
+        </Form.Item>
+        <Form.Item
+          label={
+            <span>
+              Time <BiTimer color="green" />
+            </span>
+          }
+          required
+          labelCol={{ span: 7 }}
+        >
+          <Input
+            maxLength={2}
+            style={{ width: "100%" }}
+            onChange={({ target: { value } }) => {
+              if (!isNaN(parseInt(value)))
+                setResolvedItem({ ...resolvedItem, time: parseInt(value) });
+            }}
+          />
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
--- src/components/EverHourDrop/Recenttask/index.tsx
+++ src/components/EverHourDrop/Recenttask/index.tsx
@@ -1,13 +1,10 @@
-import React, { FC, useCallback, useEffect, useState } from 'react';
+import React, { FC, useState } from 'react';
 import style from './style.module.scss';
 import { FaProjectDiagram } from "react-icons/fa";
 import 'antd/dist/antd.css';
-import { IRecentTaskProps, ITimeSlot } from '../../../models';
+import { IRecentTaskProps } from '../../../models';
 import { convertSecondsToHours, getDaysMonth } from '../utilis';
-import { DropSlot } from '../../DropSlot';
-import { IItemProps } from '../../item';
 import { WorkItemTypes } from '../../../enums';
-import { GetWorkItemType } from '../../ResolvedIsland/Workitem/utilis';
 import { TrackedItems } from './TrackedItems';
 
 
@@ -23,14 +20,9 @@ export interface IResolvedProps{
 
 export const RecentTask: FC<IRecentTaskProps> = ({ name, projectName, taskTimes, id,totalTime, week ,setIsEditing}) => {
 
-    const [activeColumn,setActiveColumn]=useState<string>();
     const [resolvedItem,setResolvedItem]=useState<IResolvedProps>();
 
 
-
-    useEffect(()=>{
-       if(resolvedItem?.details) setActiveColumn('');
-    },[resolvedItem])
 
     const weekdays = getDaysMonth(week.from, week.to);
 
@@ -44,7 +36,6 @@ export const RecentTask: FC<IRecentTaskProps> = ({ name, projectName, taskTimes,
           setIsEditing(true);
      }
 
-    console.log("active id ::",resolvedItem)
     return (
         <div className={style.everHourContainer}>
             <div className={style.RecentTask}>
