b2c07b4ced8dfa94bfc16cb0fd21e43da489a01c
Phumudzo Nthangeni <phumudzo.nthangeni@boxfusion.io>
2022-11-03 21:27:33 +0200

    re-push

---
+++ src/providers/everHour/index.tsx
@@ -0,0 +1,85 @@
+import React, { FC, useReducer, useContext, useEffect, PropsWithChildren } from 'react';
+import { useMutate } from 'restful-react';
+import { getFlagSetters } from '../utils/flagsSetters';
+import { getWeekTasksAction, getWeekTasksErrorAction, getWeekTasksSuccessAction, updateTaskAction, updateTaskErrorAction } from './actions';
+import {  EverHourActionsContext, EverHourStateContext, ITimeSlot, IWeekTasksInput } from './contexts';
+import { everHourReducer } from './reducer';
+
+
+
+const EverHourProvider:FC<PropsWithChildren<any>> = ({ children }) => {
+
+    const [state, dispatch] = useReducer(everHourReducer, {});
+    const {  mutate:fetchWeekTaskHttp} = useMutate({
+        path: "/api/everHour/getWeekTasks",
+        verb:'POST'
+      });
+      const {  mutate:updateTaskHttp} = useMutate({
+        path: "/api/everHour/updateTask",
+        verb:'PUT'
+      });
+const getWeekTasks=(payload:IWeekTasksInput)=>{
+    dispatch(getWeekTasksAction());
+    fetchWeekTaskHttp(payload)
+    .then(({timeSheets})=>{
+     
+      //@ts-ignore
+           dispatch(getWeekTasksSuccessAction(timeSheets))
+    })
+    .catch((error)=>{
+        dispatch(getWeekTasksErrorAction(error))
+    })
+
+}
+const updateTask=(payload:ITimeSlot)=>{
+  dispatch(updateTaskAction());
+  updateTaskHttp(payload)
+  .then((res)=>{
+   
+    //@ts-ignore
+         dispatch(updateTaskSuccessAction(res))
+  })
+  .catch((error)=>{
+      dispatch(updateTaskErrorAction(error))
+  })
+
+} 
+    return(
+        <EverHourStateContext.Provider value={state}>
+        <EverHourActionsContext.Provider
+          value={{
+            ...getFlagSetters(dispatch),
+            getWeekTasks,
+            updateTask
+    
+            /* NEW_ACTION_GOES_HERE */
+          }}
+        >
+          {children}
+        </EverHourActionsContext.Provider>
+      </EverHourStateContext.Provider>
+    )
+}
+
+
+function useEverHourState() {
+    const context = useContext(EverHourStateContext);
+    if (!context) {
+      throw new Error('useEverHourState must be used within a EverHourProvider');
+    }
+    return context;
+  }
+  
+  function useEverHourActions() {
+    const context = useContext(EverHourActionsContext);
+    if (context === undefined) {
+      throw new Error('useEverHourActions must be used within a EverHourProvider');
+    }
+    return context;
+  }
+  
+  function useEverHour() {
+    return { ...useEverHourActions(), ...useEverHourState() };
+  }
+  
+  export { EverHourProvider, useEverHourActions, useEverHourState, useEverHour };
---
+++ src/providers/everHour/reducer.ts
@@ -0,0 +1,45 @@
+import { EverHourActionEnums } from "./actions";
+import { IEverHourStateContext } from "./contexts";
+import flagsReducer from "../utils/flagsReducer";
+
+export function everHourReducer(
+  incomingState: IEverHourStateContext,
+  action: ReduxActions.Action<IEverHourStateContext>
+): IEverHourStateContext {
+  //#region Register flags reducer
+  const state = flagsReducer(incomingState, action);
+
+  const { type, payload } = action;
+  //#endregion
+
+  switch (type) {
+    case EverHourActionEnums.GetWeekTasksRequest:
+    case EverHourActionEnums.GetWeekTasksSuccess:
+    case EverHourActionEnums.GetWeekTasksError:
+    case EverHourActionEnums.UpdateTaskRequest:
+    case EverHourActionEnums.UpdateTaskSuccess:
+      // const {timeSheets}=state;
+      // const {timeSlot:{date}}=payload;
+      // const index=(timeSheets.filter(({dailyTimes})=>{
+      //   return dailyTimes.includes(date);
+      // }))
+      // console.log('time ::',index)
+      return {
+        ...state,
+        ...payload,
+      };
+
+    case EverHourActionEnums.UpdateTasksError:
+    case EverHourActionEnums.UpdateSlot:
+      /* NEW_ACTION_ENUM_GOES_HERE */
+      //#endregion
+      return {
+        ...state,
+        ...payload,
+      };
+
+    default: {
+      return state;
+    }
+  }
+}
