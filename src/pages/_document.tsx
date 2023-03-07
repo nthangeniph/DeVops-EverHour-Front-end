ed22c30dfb1c60ae3eda891155ccc708287d69b0
Phumudzo <phumudzon@boxfusion.co.za>
2023-03-07 10:27:01 +0200

    website header added

---
+++ src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+          <Head>
+          <title>DeVops_EverHour Integration</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
